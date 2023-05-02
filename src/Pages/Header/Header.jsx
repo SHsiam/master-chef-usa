0d767cfeb571f6ac78bda4e687dcb8a0cdacd2f8
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-05-03 03:00:06 +0600

    navbar done

---
+++ src/Pages/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <>
+        <Navbar bg="light" variant="light">
+          <Container>
+            <Navbar.Brand  href="#home">Master Chef USA</Navbar.Brand>
+            <Nav className="d-flex justify-content-between">
+              <Nav.Link>
+                <Link className='text-decoration-none text-dark' to='/'>Home</Link>
+              </Nav.Link>
+              <Nav.Link>
+              <Link className='text-decoration-none text-dark' to='/blog'>Blog</Link>
+              </Nav.Link>
+              <Nav.Link>
+              <Link className='text-decoration-none text-dark' to='/profile'>Profile</Link>
+              </Nav.Link>
+              <Nav.Link>
+              <button class="btn btn-outline-success">
+              <Link className='text-decoration-none text-dark' to='/login'>Login</Link>
+              </button>
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+     
+      </>
+    );
+};
+
+export default Header;
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from '../Pages/Home/Home/Home';
+import Header from '../Pages/Header/Header';
+import Footer from '../Pages/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Home></Home>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
