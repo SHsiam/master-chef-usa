44956ef8981a69fabd8d81f7c948b8726fe39d9b
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-05-04 13:52:33 +0600

    enviorment done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAZSH1FsC9kCTkYqWFVCgL63ZFYBEW6j_0",
-  authDomain: "master-chef-usa.firebaseapp.com",
-  projectId: "master-chef-usa",
-  storageBucket: "master-chef-usa.appspot.com",
-  messagingSenderId: "407226301420",
-  appId: "1:407226301420:web:77fc117c43ccd338ac9ec9"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
